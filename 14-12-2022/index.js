0839efb73cec4dbb96a522b72738f25da57519bf
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-14 12:36:47 +0500

    14-12-2022

---
+++ 14-12-2022/index.js
@@ -0,0 +1,45 @@
+// var sum=0;
+// for(var i=100;i<=200;i++){
+//     if(i%9===0){
+//         sum=sum+i
+//     }
+// }
+
+// document.write("The sum is: "+sum)
+
+
+// var name="talha"
+// var email="someone@example.com"
+// var age=20
+// var batch="2210B1"
+// var studentNumber=12345
+
+// document.write(name)
+// document.write(age)
+
+
+
+
+// var studentData=["talha","someone@example.com",20,"2210B1",12345]
+
+
+// for(i=0;i<studentData.length;i++){
+//     document.write(studentData[i]+"<br>")
+// }
+
+var numbers=[1,2,3,4,5]
+
+// document.write(numbers[5])
+var sum=0
+for(var i=0;i<=numbers.length;i++){
+sum=numbers[i]+sum
+}
+
+document.write(sum)
+
+
+
+
+
+
+
